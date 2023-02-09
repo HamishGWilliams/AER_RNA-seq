b1c04e1430db5cf255ce18f691fff7e690443fed
Hamish Gruffydd Williams <122924272+HamishGWilliams@users.noreply.github.com>
2023-02-09 12:23:30 +0000

    hashed out text

--- UNIX/scripts/FeatureCounts_Script.sh
+++ UNIX/scripts/FeatureCounts_Script.sh
@@ -17,22 +17,22 @@ echo "Counts read!"
 
 ### Options used:
 # -p: If specified, libraries are assumed to contain paired-end
-                      reads. For any library that contains paired-end reads, the
-                      'countReadPairs' parameter controls if read pairs or reads
-                      should be counted.
+                     # reads. For any library that contains paired-end reads, the
+                      #'countReadPairs' parameter controls if read pairs or reads
+                      # should be counted.
 
 # -g:  Specify attribute type in GTF annotation. 'gene_id' by
-                      default. Meta-features used for read counting will be
-                      extracted from annotation using the provided value.
+                      ##default. Meta-features used for read counting will be
+                      # extracted from annotation using the provided value.
 
 			## 'Parent' should be specified.
 
 
 # -t: Specify feature type(s) in a GTF annotation. If multiple
-                      types are provided, they should be separated by ',' with
-                      no space in between. 'exon' by default. Rows in the
-                      annotation with a matched feature will be extracted and
-                      used for read mapping.
+                     # types are provided, they should be separated by ',' with
+                     # no space in between. 'exon' by default. Rows in the
+                     # annotation with a matched feature will be extracted and
+                     # used for read mapping.
 
 # -O: Assign reads to all their overlapping meta-features (or
-                      features if -f is specified).
+                     # features if -f is specified).
